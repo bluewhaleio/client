594fc164702ab9a6de0c75cc2b667ce8488bdd55
bluewhaleio <bluewhale>
2017-08-19 22:34:00 +0000

    new code

---
+++ src/main/java/com/fmr/AP1234/App.java
@@ -0,0 +1,16 @@
+package com.fmr.AP1234;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Utils u = new Utils("sridhar");
+
+        System.out.println(u.getName());
+    }
+}
