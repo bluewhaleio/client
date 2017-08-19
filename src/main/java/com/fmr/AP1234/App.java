6047949b21020991d89c79513d6acf221af92711
bluewhaleio <bluewhale>
2017-08-19 22:58:41 +0000

    x

--- src/main/java/com/fmr/AP1234/App.java
+++ src/main/java/com/fmr/AP1234/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Utils u = new Utils("sridhar");
+        Utils u = new Utils("sridhar client");
 
         System.out.println(u.getName());
     }
